cedd697d323b6942320e686579986f3f4212a5b5
flormarianne <2015081647@ust-ics.mygbiz.com>
2018-11-26 10:39:27 +0800

    2nd commit

--- app/src/main/java/ph/edu/ust/jatallas/project/Mocktails.java
+++ app/src/main/java/ph/edu/ust/jatallas/project/Mocktails.java
@@ -1,13 +1,90 @@
 package ph.edu.ust.jatallas.project;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.app.SearchManager;
+import android.support.v7.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
+
+import java.util.ArrayList;
 
 public class Mocktails extends AppCompatActivity {
+
+    ListView listView;
+    AdapterMocktailsListView adapter;
+    String[] title;
+    String[] description;
+    int[] icon;
+    ArrayList<ModelMocktails> arrayList = new ArrayList<ModelMocktails>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mocktails);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("MOCKTAILS ");
+
+        title = new String[]{"Gong Cha UST The One","D'Cream P.Noval", "Moonleaf Espana", "I love Milktea Sampaloc", "Bon Appetea Espana" };
+        description = new String[]{"Location: Food Court, Second Floor, The One Grand Centre, España", "Location: P. Noval Street, Near Espana Boulevard, Sampaloc, Manila",
+                "Location: G/F, St Thomas Square, España Blvd, 397, Manila, 1015 Metro Manila", "Location: Padre Noval Street, Near UST Campus, Sampaloc, Manila",
+                "Location: 838 P. Campa Street, Sampaloc, Manila"};
+        icon = new int[]{R.drawable.milkgongcha , R.drawable.milkdcream, R.drawable.milkmoonleaf, R.drawable.milkilmilktea, R.drawable.milkbonappetea};
+
+        listView = findViewById(R.id.listView);
+
+        for (int i = 0; i<title.length; i++){
+            ModelMocktails modelMocktails = new ModelMocktails(title[i], description[i],icon[i] );
+            //bind strings in array
+            arrayList.add(modelMocktails);
+        }
+
+        //pass results to AdapterMocktailsListView
+        adapter = new AdapterMocktailsListView(this, arrayList);
+
+        //bind adapter in list view
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menumocktails, menu);
+
+        MenuItem myActionMenuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView)myActionMenuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                if (TextUtils.isEmpty(s)) {
+                    adapter.filter("");
+                    listView.clearTextFilter();
+                }
+                else {
+                    adapter.filter(s);
+                }
+
+                return true;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/ph/edu/ust/jatallas/project/Soda.java
+++ app/src/main/java/ph/edu/ust/jatallas/project/Soda.java
@@ -1,34 +1,90 @@
 package ph.edu.ust.jatallas.project;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.app.SearchManager;
+import android.support.v7.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
 
-public class Soda extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+import java.util.ArrayList;
+
+public class Soda extends AppCompatActivity {
+
+    ListView listView;
+    AdapterSodaListView adapter;
+    String[] title;
+    String[] description;
+    int[] icon;
+    ArrayList<ModelSoda> arrayList = new ArrayList<ModelSoda>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soda);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("SMOOTHIES ");
 
-        Spinner spinner = findViewById(R.id.planets_spinner);
-        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.price, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
+        title = new String[]{"Gong Cha UST The One","D'Cream P.Noval", "Moonleaf Espana", "I love Milktea Sampaloc", "Bon Appetea Espana" };
+        description = new String[]{"Location: Food Court, Second Floor, The One Grand Centre, España", "Location: P. Noval Street, Near Espana Boulevard, Sampaloc, Manila",
+                "Location: G/F, St Thomas Square, España Blvd, 397, Manila, 1015 Metro Manila", "Location: Padre Noval Street, Near UST Campus, Sampaloc, Manila",
+                "Location: 838 P. Campa Street, Sampaloc, Manila"};
+        icon = new int[]{R.drawable.milkgongcha , R.drawable.milkdcream, R.drawable.milkmoonleaf, R.drawable.milkilmilktea, R.drawable.milkbonappetea};
 
+        listView = findViewById(R.id.listView);
+
+        for (int i = 0; i<title.length; i++){
+            ModelSoda modelSoda = new ModelSoda(title[i], description[i],icon[i] );
+            //bind strings in array
+            arrayList.add(modelSoda);
+        }
+
+        //pass results to AdapterSodaListView
+        adapter = new AdapterSodaListView(this, arrayList);
+
+        //bind adapter in list view
+        listView.setAdapter(adapter);
     }
 
     @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menusoda, menu);
+
+        MenuItem myActionMenuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView)myActionMenuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                if (TextUtils.isEmpty(s)) {
+                    adapter.filter("");
+                    listView.clearTextFilter();
+                }
+                else {
+                    adapter.filter(s);
+                }
+
+                return true;
+            }
+        });
+        return true;
     }
 
     @Override
-    public void onNothingSelected(AdapterView<?> parent) {
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
 
+        if (id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
